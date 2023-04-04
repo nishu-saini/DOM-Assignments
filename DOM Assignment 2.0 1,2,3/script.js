79563a8a3bdaf80413e8744a9656fc3e11ed97a0
quarnstric <nishusainialwar@gmail.com>
2023-04-04 17:11:56 +0530

    Assignment 1 Completed

---
+++ DOM Assignment 2.0 1,2,3/script.js
@@ -0,0 +1,56 @@
+// Assignment 1
+
+/*
+// Task 1
+const ul = document.querySelector('ul');
+
+// create li element..
+let li = document.createElement('li');
+li.innerHTML = '<a href="#">Hire Me</a>';
+
+// add child to ul..
+ul.appendChild(li);
+*/
+
+/*
+// Task 2
+const items = document.querySelectorAll("nav ul li");
+items[2].innerText = "Projects";
+
+const input = document.querySelector(".search-field input");
+input.placeholder = "Search My Project";
+*/
+
+/*
+// Task 3
+const items = document.querySelectorAll("nav ul li");
+items[2].innerText = "Projects";
+
+// select all spans which are inside hero-left-section > p
+const spans = document.querySelectorAll(".hero-left-section p span");
+spans[1].innerText = 'an Employee';
+spans[2].innerText = 'iNeuron Intelligence Pvt Ltd';
+*/
+
+/*
+// Task 4
+const items = document.querySelectorAll("nav ul li");
+items[2].innerText = "Projects";
+
+const image = document.querySelector('.hero-right-section img');
+image.src = "./hitesh_sir.png";
+*/
+
+/*
+// Task 5
+const items = document.querySelectorAll("nav ul li");
+items[2].innerText = "Projects";
+
+const new_btn = document.createElement('button');
+new_btn.innerText = "Support Me";
+
+// add new_btn to dom..
+let btn_section = document.querySelector(".hero-right-section-btns");
+btn_section.appendChild(new_btn);
+*/
+
