185af700a94c0a5eb9f7684c67641aac1caccff4
quarnstric <nishusainialwar@gmail.com>
2023-04-05 15:12:40 +0530

    Assignment 6 completed

---
+++ 06_DOM Project/06_DOM Project/script.js
@@ -0,0 +1,10 @@
+// Assignment 6
+// Task 1
+let logo = document.querySelector("header img");
+// change logo..
+logo.src = "./assets/ineuron-logo.png";
+
+
+// Task 2
+let app_price = document.querySelector(".app_price span");
+app_price.textContent = "$10";
