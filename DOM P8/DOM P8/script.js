879cc64acc5243a6388f5e543861534e4694dfd3
quarnstric <nishusainialwar@gmail.com>
2023-04-08 22:42:20 +0530

    Assignment 8 Completed

---
+++ DOM P8/DOM P8/script.js
@@ -0,0 +1,42 @@
+// Assignment 8
+// Task 1
+/*
+let aside = document.querySelector("aside");
+aside.style.height = "auto";
+
+// create elements..
+let hr = document.createElement("hr");
+hr.className = "hr-line";
+
+let new_h2 = document.createElement("h2");
+new_h2.className = "new-head";
+new_h2.textContent = "This is my custom heading";
+
+let new_p = document.querySelector("p");
+new_p.className = "new-p";
+new_p.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt et iusto quod nostrum cupiditate corrupti.";
+
+// append new element to aside..
+aside.appendChild(hr);
+aside.appendChild(new_h2);
+aside.appendChild(new_p);
+*/
+
+// Task 2
+// document.body.style.background = 'none';
+
+// Task 3
+/*
+// get the navbar toggler button..
+let navbarToggler = document.querySelector(".navbar-toggler");
+
+// get the collapse elements..
+let navbarCollapse = document.querySelector(".navbar-collapse");
+
+// add click event listener to the navbar toggler button.
+navbarToggler.addEventListener('click', function() {
+    // Toggle the 'show' class on the collapse element..
+    navbarCollapse.classList.toggle('show');
+
+});
+*/
