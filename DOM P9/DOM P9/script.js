18bef5e74e63b1b42ffc695343b171fa4ac98510
quarnstric <nishusainialwar@gmail.com>
2023-04-08 23:05:32 +0530

    Assignment 9 completed

---
+++ DOM P9/DOM P9/script.js
@@ -0,0 +1,24 @@
+// Assignment 9
+
+// Task 1
+/*
+let heading = document.querySelector(".caption h1");
+heading.style.color = "#c93d44";
+*/
+
+// Task 2
+/*
+let cart_btn = document.querySelector(".add-to-cart");
+
+// add mouseenter event listener to cart button..
+cart_btn.addEventListener('mouseenter', function() {
+    // change element background..
+    this.style.background = "#c92d44";
+});
+
+// add mouseleave listener to cart button..
+cart_btn.addEventListener('mouseleave', function() {
+    // change element color back to normal..
+    this.style.background = "#3c8067";
+});
+*/
